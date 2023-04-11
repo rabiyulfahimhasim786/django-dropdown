e33d748bad0eed15c8e9dd03cb33effe566f3e83
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2023-04-11 08:14:47 -0500

    Delete dropdown directory

--- dropdown/persons/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from persons.models import City, Country, Person
-
-admin.site.register(City)
-admin.site.register(Country)
-admin.site.register(Person)
